46a1b9da09d4c206b20f75c55be9cc4be104d9cd
Siddhant Shah <sshah14@ncsu.edu>
2018-11-13 14:12:40 -0500

    Finishing up Customer Views prototype

--- src/view/Customer.java
+++ src/view/Customer.java
@@ -161,32 +161,132 @@ public class Customer {
 		}	 
 		 
 	 }
-	 public static void scheduleMaintenancePage2() {
-		 
+	 
+	 public static void scheduleMaintenancePage2(Scanner sc) {
+		 // Display the two identified service dates and mechanic name found based on the inputs in the
+		 // previous page to the user, followed by the menu.
+		System.out.println("======================Schedule Maintenance Page 2======================");
+		// Take input and schedule service 
+		System.out.println("1. Schedule on Date");
+		System.out.println("2. Go Back");
+		System.out.println("Please select your choice.");
+		int choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			// see documentation;
+		case 2:
+			// exit/go back to displayLandingPage
+		}	 
+	 }
+	 
+	 public static void scheduleRepairPage1(Scanner sc) {
+		 // Display the menu to allow the user to pick on of the possible problems.
+		System.out.println("======================Schedule Repair Page 1======================");
+		System.out.println("1. Engine knock");
+		System.out.println("2. Car drifts in a particular direction");
+		System.out.println("3. Battery does not hold charge");
+		System.out.println("4. Black/unclean exhaust");
+		System.out.println("5. A/C-Heater not working");
+		System.out.println("6. Headlamps/Tail lamps not working");
+		System.out.println("7. Check engine light");
+		System.out.println("8. Go Back");
+		System.out.println("Please select your choice.");
+		int choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			// Engine knock
+		case 2:
+			// Car drifts in a particular direction
+		case 3:
+			// Battery does not hold charge
+		case 4:
+			// Black/unclean exhaust
+		case 5:
+			// A/C-Heater not working
+		case 6:
+			// Headlamps/Tail lamps not working
+		case 7:
+			// Check engine light
+		case 8:
+			// exit/go back 
+		}	
 		 
 	 }
-	 public static void scheduleRepairPage1() {
-		 
+	 
+	 public static void scheduleRepairPage2(Scanner sc) {
+		// Display the diagnostic report and the two identified service dates and mechanic name found based on the inputs in the previous page to the user, followed by the menu.	 
+		System.out.println("======================Schedule Repair Page 2======================");
+		System.out.println("1. Repair on Date");
+		System.out.println("2. Go Back");
+		System.out.println("Please select your choice.");
+		int choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			// Repair on Date
+		case 2:
+			// exit/go back 
+		}	 
+	 }
+	 
+	 public static void rescheduleServicePage1(Scanner sc) {
+		 // Display the following details for all upcoming services for this customer, followed by the menu
+		 // A. License Plate B. Service ID C. Service Date D. Service Type (Maintenance/Re pair) E. Service Details (Service A/B/C or Problem)
+	 	System.out.println("======================Reschedule Service Page 1======================");
+		System.out.println("1. Pick a service");
+		System.out.println("2. Go Back");
+		System.out.println("Please select your choice.");
+		int choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			// Pick a service
+		case 2:
+			// exit/go back 
+		}	
 		 
 	 }
-	 public static void scheduleRepairPage2() {
-		 
+	 public static void rescheduleServicePage2(Scanner sc) {
+		 // Display the two identified service dates and mechanic name, followed by the menu.
+		 	System.out.println("======================Reschedule Service Page 2======================");
+			System.out.println("1. Reschedule Date");
+			System.out.println("2. Go Back");
+			System.out.println("Please select your choice.");
+			int choice = sc.nextInt();
+			switch(choice) {
+			case 1:
+				// Reschedule Date
+			case 2:
+				// exit/go back 
+			}	
 		 
 	 }
-	 public static void rescheduleServicePage1() {
-		 
-		 
+	 
+	 public static void invoice(Scanner sc) {
+		 // Display the following details for all the services that are complete followed by the menu. A. Service ID B. Service Start Date/Time
+		 // C. Service End Date/Time D. Licence Plate E. Service Type F. Mechanic Name G. Total
+		System.out.println("======================Invoice======================");
+		System.out.println("1. View Invoice Details");
+		System.out.println("2. Go Back");
+		System.out.println("Please select your choice.");
+		int choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			// View Invoice Details
+		case 2:
+			// exit/go back to Customer: Landing page
+		}	
 	 }
-	 public static void rescheduleServicePage2() {
-		 
-		 
-	 }
-	 public static void invoice() {
-		 
-		 
-	 }
-	 public static void viewInvoiceDetails() {
-		 
+	 
+	 public static void viewInvoiceDetails(Scanner sc) {
+		 // Ask user to input the following detail in order to show the described output followed by an option to go back as shown under “Menu”. A. Service ID
+		System.out.println("======================View Invoice Details======================");
+		// Take input
+		System.out.println("1. Go Back");
+		System.out.println("Please select your choice.");
+		int choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			// exit/go back to Customer: Landing page
+		}	
 		 
 	 }
 }
